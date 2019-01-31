5aadb0f09cf1903a794341ae551bd560e3796ec3
kriskbx <m@kris.cool>
2019-01-31 14:04:51 +0100

    forgot to turn off debug mode again...

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ let gtt = new events.EventEmitter(),
     settingsWindow = null,
     aboutWindow = null,
     listWindow = null,
-    debug = true;
+    debug = false;
 
 gtt._app = app;
 gtt._version = pjson.version;
